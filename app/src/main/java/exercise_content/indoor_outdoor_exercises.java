c44ffd6c550a9b98a2bcb4154cf215e2d29eabd5
Clydpogi_Justin <justinorendain0801@gmail.com>
2024-10-20 18:06:52 +0800

    exercise content available

---
+++ app/src/main/java/exercise_content/indoor_outdoor_exercises.java
@@ -0,0 +1,66 @@
+package exercise_content;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pethub.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link indoor_outdoor_exercises#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class indoor_outdoor_exercises extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public indoor_outdoor_exercises() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment indoor_outdoor_exercises.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static indoor_outdoor_exercises newInstance(String param1, String param2) {
+        indoor_outdoor_exercises fragment = new indoor_outdoor_exercises();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_indoor_outdoor_exercises, container, false);
+    }
+}
---
+++ app/src/main/java/exercise_content/types_of_exercise.java
@@ -0,0 +1,66 @@
+package exercise_content;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pethub.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link types_of_exercise#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class types_of_exercise extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public types_of_exercise() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment types_of_exercise.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static types_of_exercise newInstance(String param1, String param2) {
+        types_of_exercise fragment = new types_of_exercise();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_types_of_exercise, container, false);
+    }
+}
