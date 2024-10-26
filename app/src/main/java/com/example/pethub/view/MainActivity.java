3ddc30fc2a2b85bc9e01055669f10f2ead12fe84
Julius Jervin V. Bundalian <julius.jervin@gmail.com>
2024-10-26 18:30:58 +0800

    ui updates

--- app/src/main/java/com/example/pethub/view/MainActivity.java
+++ app/src/main/java/com/example/pethub/view/MainActivity.java
@@ -7,20 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.pethub.databinding.ActivityMainBinding;
 
 import com.example.pethub.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding; // Binding object
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize View Binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Set up NavController
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(binding.navHostFragment.getId());
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null; // Avoid memory leaks
     }
 }
--- app/src/main/java/com/example/pethub/view/ViewHolderFragment.java
+++ app/src/main/java/com/example/pethub/view/ViewHolderFragment.java
@@ -3,64 +3,62 @@ package com.example.pethub.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pethub.R;
+import com.example.pethub.view.contacts.ContactsFragment;
+import com.example.pethub.view.home.HomeFragment;
+import com.example.pethub.view.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ViewHolderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class ViewHolderFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ViewHolderFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewHolderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ViewHolderFragment newInstance(String param1, String param2) {
-        ViewHolderFragment fragment = new ViewHolderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private BottomNavigationView bottomNavigationView;
+    private final Map<Integer, Fragment> fragmentMap = new HashMap<>(); // Map for menu item IDs to fragments
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_holder, container, false);
+
+        bottomNavigationView = view.findViewById(R.id.bottom_navigationUser);
+
+        // Initialize the fragment map
+        fragmentMap.put(R.id.navigation_home, new HomeFragment());
+        fragmentMap.put(R.id.navigation_contact, new ContactsFragment());
+        fragmentMap.put(R.id.navigation_profile, new ProfileFragment());
+
+        // Set default fragment
+        loadFragment(fragmentMap.get(R.id.navigation_home));
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment selectedFragment = fragmentMap.get(item.getItemId()); // Get the corresponding fragment
+                return loadFragment(selectedFragment); // Load the selected fragment
+            }
+        });
+
+        return view;
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        // Replace the current fragment with the selected one
+        if (fragment != null) {
+            FragmentManager fragmentManager = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            return true;
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_holder, container, false);
+        return false;
     }
 }
