77cec3fe3d4a87fa2f9c16e3d4325d6a58df6643
Ashleya10 <shogunbundalian5@gmail.com>
2024-10-28 17:47:08 +0800

    Nutrition, Exercise and Health page

---
+++ app/src/main/java/com/example/pethub/view/home/health/HealthCommonIssuesFragment.java
@@ -0,0 +1,21 @@
+package com.example.pethub.view.home.health;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pethub.R;
+
+public class HealthCommonIssuesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_health_common_issues, container, false);
+    }
+}
